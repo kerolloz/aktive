59b45411a91d9b57ef3829902e6ad10cccda181d
Kerollos Magdy <kerolloz@yahoo.com>
2023-06-09 17:23:23 +0300

    use up-to-date data from committers.top

--- rank.js
+++ rank.js
@@ -1,20 +1,16 @@
 const axios = require("axios");
-const atob = require("atob");
 const ordinal = require("ordinal");
 const YAML = require("yamljs");
 
 async function getOrdinalRank(username, country) {
-  const githubTopRepo = "lauripiispanen/github-top";
-  const countryPath = `_data/locations/${country}.yml`;
+  const githubTopRepo = "ashkulz/committers.top";
+  const countryPath = `gh-pages/_data/locations/${country}.yml`;
 
   const { data } = await axios({
-    url: `https://api.github.com/repos/${githubTopRepo}/contents/${countryPath}`,
+    url: `https://raw.githubusercontent.com/${githubTopRepo}/${countryPath}`,
   });
 
-  const decodedContent = atob(data.content);
-  const userData = decodedContent
-    .split("\n\n")
-    .find((u) => u.includes(username));
+  const userData = data.split("\n\n").find((u) => u.includes(username));
   const json = YAML.parse(userData)[0];
   return ordinal(json.rank);
 }
